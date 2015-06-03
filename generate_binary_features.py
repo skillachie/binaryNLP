64ed45b9da2115a2397a660ca57addbb2e9b391f
Skillachie <dwaynecampbell13@gmail.com>
2015-06-03 16:57:07 -0400

    disable continuous features from master

--- generate_binary_features.py
+++ generate_binary_features.py
@@ -38,11 +38,15 @@ def text_classif_features(directory,match,start,end,outpath,cpus,aggr_freq,add_n
       event_counts.append(result)
 
 
+    # Event counts
     event_series = CategorySeries(start,end,aggr_freq,add_noise)
-    event_results = event_series.get_event_timeseries(event_counts)
-    event_results.to_csv('event_features.csv') 
+    event_count_results = event_series.get_event_timeseries(event_counts)
+    event_count_results.to_csv('event_features_count.csv') 
+
+    # Continuos Events
+    #event_countinuous_results = event_series.get_continuous_events_timeseries(event_counts,5,'daily')
    
-    mpd = series_result.join(event_results, how='outer')
+    mpd = series_result.join(event_count_results, how='outer')
     mpd.to_csv(outpath)
 
 def split_date(date_str):
